3fb24b0182a81c5abe3e0e4da75800c6561ec6ef
marishanmugam <mageshg1402@gmail.com>
2022-07-18 16:41:28 +0530

    Tenor web clone

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './images/tenor-logo.svg';
-import './App.css';
-import Search from './Search.js';
-import Trending from './Trending';
-
+import logo from "./images/tenor-logo.svg";
+import "./App.css";
+import Search from "./Search.js";
+import Trending from "./Trending";
 
 function App() {
   return (
     <div className="head">
       <header className="head-container">
-      <img src={logo}></img>
+        <img src={logo}></img>
       </header>
-    
-    <div className='search'>
-      <Search />
+
+      <div className="search">
+        <Search />
       </div>
 
-      <div className='trending'>
+      <div className="trending">
         <Trending />
       </div>
     </div>
--- src/Trending.js
+++ src/Trending.js
@@ -1,104 +1,93 @@
-import React, { useState,useEffect } from 'react'
-import './trending.css';    
+import React, { useState, useEffect } from "react";
+import "./trending.css";
 import axios from "axios";
-import Search from './Search';
+import Search from "./Search";
 
 function Trending() {
+  const [featured, setFeatured] = useState("[]");
+  const [ser, setSer] = useState([]);
+  const [data, setdata] = useState([]);
+  const [trend, setTrend] = useState([]);
+  const [index, setIndex] = useState(0);
 
-    const [featured,setFeatured] = useState('[]');
-    const [ser,setSer] = useState ([]);
-    const [data,setdata] =useState ([])
-    const [trend,setTrend] =useState([]);
-    const [index,setIndex ] = useState(0);
+  var noOfSlide = ser.length;
+  console.log(noOfSlide);
 
-    var noOfSlide = ser.length;
-    console.log(noOfSlide)
+  const fetchData = () => {
+    axios
+      .get(
+        "https://tenor.googleapis.com/v2/trending_terms?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c"
+      )
+      .then((res) => {
+        setFeatured(res.data.results);
+      });
+  };
+  // console.log(featured);
 
-    const fetchData = () => {
-        axios
-        .get ('https://tenor.googleapis.com/v2/trending_terms?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c')
-        .then((res) => {
-            
+  // const arr = [];
+  // featured.forEach((data,id) => {
+  //     <div key={id}>
+  //     arr.push({data});</div>
+  // });
 
-            setFeatured(res.data.results);
-          
-           
-        });
-    }
-    // console.log(featured);
+  // console.log(arr)
 
-   
+  const searchData = () => {
+    axios
+      .get(
+        `https://tenor.googleapis.com/v2/search?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c&q=${featured}`
+      )
 
-    // const arr = [];
-    // featured.forEach((data,id) => {
-    //     <div key={id}>
-    //     arr.push({data});</div>
-    // });
+      .then((res1) => {
+        setSer(res1.data.results);
+      });
+  };
+  console.log(ser);
+  useEffect(() => {
+    fetchData();
+    searchData();
+  }, []);
 
-    // console.log(arr)
+  const prev = () => {
+    console.log("pre Clicked");
+  };
 
-
-
-    const searchData = () => {
-        axios.get(`https://tenor.googleapis.com/v2/search?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c&q=${featured}`)
-        
-        .then((res1) => {
-       
-            setSer(res1.data.results)
-           
-           
-        });
-    }
-    console.log(ser)
-    useEffect(() => {
-        fetchData();
-        searchData();  
-      
-    },[])
-    
-const prev = () => {
-    console.log('pre Clicked')   
-    
-}
-
-const next = () => {
-    console.log('next Clicked');
-}
- 
+  const next = () => {
+    console.log("next Clicked");
+  };
 
   return (
-    <div className='trending-main'>
-        <div className='prev' >
-           <span id='prev' onClick={prev}>&#10094;</span>
-        </div>
-        <div id='next' className='next' >
-         <span id ='next' onClick={next}>&#10095;</span> 
-        </div>
-        <div className='trending-container'> 
-       
-      <h2>  Trending Tenor Searches</h2>
+    <div className="trending-main">
+      <div className="prev">
+        <span id="prev" onClick={prev}>
+          &#10094;
+        </span>
       </div>
-        <div className='trend-container'>
-          
+      <div id="next" className="next">
+        <span id="next" onClick={next}>
+          &#10095;
+        </span>
+      </div>
+      <div className="trending-container">
+        <h2> Trending Tenor Searches</h2>
+      </div>
+      <div className="trend-container">
         <span></span>
-     {
-        ser.map((e) => {
-            return (
-                <div className='trend-images' key={e.id}>
-                    <div className='trend'>
-                        <a href='#'> <img src={e.media_formats.gif.url}></img> </a>
-                    </div>  
-                </div>
-                
-                
-              
-            )
-        })
+        {ser.map((e) => {
+          return (
+            <div className="trend-images" key={e.id}>
+              <div className="trend">
+                <a href="#">
+                  {" "}
+                  <img src={e.media_formats.gif.url}></img>{" "}
+                </a>
+              </div>
+            </div>
+          );
+        })}
+        <span>{trend}</span>
 
-        }
-<span>{trend}</span>
-
-     {/* {
+        {/* {
          featured.map((index,id) => {
             return (
                 <div className=''>
@@ -109,11 +98,9 @@ const next = () => {
         })
      }
       */}
-
-
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Trending
+export default Trending;
