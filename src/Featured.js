2d2466fe9e697b8136df04058c5fdc4037a2866b
marishanmugam <mageshg1402@gmail.com>
2022-07-18 19:01:08 +0530

    tenor web app

--- src/Featured.js
+++ src/Featured.js
@@ -1,53 +1,45 @@
-import React, { useState } from 'react'
-import './featured.css'
-import axios from 'axios'
-import {useEffect} from 'react';
-
+import React, { useState } from "react";
+import "./featured.css";
+import axios from "axios";
+import { useEffect } from "react";
 
 function Featured() {
+  const [featured, setFeatured] = useState([]);
 
-    const [featured,setFeatured] = useState([]);
+  const fetchData = () => {
+    axios
+      .get(
+        "https://tenor.googleapis.com/v2/featured?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c"
+      )
+      .then((res) => {
+        setFeatured(res.data.results);
+        console.log(setFeatured);
+      });
+  };
 
-    const fetchData = () => {
-        axios
-        .get('https://tenor.googleapis.com/v2/featured?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c')
-        .then((res) => {
-            setFeatured(res.data.results);
-            console.log(setFeatured)
-        })
-    } 
-
-   
-
-    useEffect(() => {
-     fetchData();
-    }, [])
-    
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div className='featured-container'>
-        <div className='featured-content'>
-          <h2>Featured GIFs</h2>
+    <div className="featured-container">
+      <div className="featured-content">
+        <h2>Featured GIFs</h2>
+
+        <div className="image-container">
+            <div className="coloum"></div>
+          {featured.map((e) => {
+            return (
+              <div className="images" key={e.id}>
+                <img src={e.media_formats.gif.url} style={{width :'100%'}}></img>
+             
+              </div>
+            );
+          })}
         </div>
-        <div className='image-container'>
-
-
-            {
-                featured.map((e) =>{
-                    return (
-                        <div className='images' key={e.id}>
-                        <img src={e.media_formats.gif.url}></img>
-                        </div>
-                    )
-                   
-                })
-            }
-            </div>
-
-        
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Featured
+export default Featured;
