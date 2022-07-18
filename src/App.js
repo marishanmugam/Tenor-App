805235cb5b54dc34b5628728ab9b7802316d6e11
marishanmugam <mageshg1402@gmail.com>
2022-07-18 16:37:30 +0530

    hai

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './images/tenor-logo.svg';
+import './App.css';
+import Search from './Search.js';
+import Trending from './Trending';
+
+
+function App() {
+  return (
+    <div className="head">
+      <header className="head-container">
+      <img src={logo}></img>
+      </header>
+    
+    <div className='search'>
+      <Search />
+      </div>
+
+      <div className='trending'>
+        <Trending />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Search.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './search.css'
+import whiteLogo from './images/tenor-logo-white.svg'
+import {useState,useEffect} from 'react'; 
+import searchlogo from './images/search.svg'
+
+function Search() {
+
+const [fix ,setFix] = useState(false);
+
+
+useEffect(() => {
+    const setFixed = () => {
+        if(window.scrollY >= 25)
+       setFix(true)
+        else
+        setFix(false)
+    }
+
+    window.addEventListener('scroll',setFixed)
+}, [])
+
+
+  return (
+    <div className='main-search'>
+        <div className='searh-contianer' style={{marginTop : fix ? -37 : 0 } }>
+        
+            <div className='search-box' >
+            <img src={whiteLogo} />
+                <input placeholder='Search for GIFs and Stickers' type='text' className='text-box' style={{width : fix ? '53%' : '58%',marginLeft : fix ? 72 : 0}}></input>
+                <div className='search-logo'>
+                    <img src={searchlogo}></img>    
+                 </div>
+            </div>
+        </div>
+     
+    </div>
+  )
+}
+
+export default Search
