e47ad84ca0aeccb84d7b642c09bb517eeb46c351
marishanmugam <mageshg1402@gmail.com>
2022-07-19 20:14:55 +0530

    Tenor web clone

--- src/Featured.js
+++ src/Featured.js
@@ -6,39 +6,46 @@ import { useEffect } from "react";
 function Featured() {
   const [featured, setFeatured] = useState([]);
   const [nextPos, setNextPos] = useState('');
+  const [loading,setLoading ] = useState(true)
+  const [page,setPage] = useState(1)
 
  
   useEffect(() => {
-    featuredData();
+   
+    const featuredData = () => {
+      let newData = [];
+      axios
+        .get(
+          `https://tenor.googleapis.com/v2/featured?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c&pos=${nextPos}&limit=5`
+        )
+  
+        .then((res) => {
+          res.data.results.map((res) => newData.push(res));
+          setFeatured((oldData) => [...oldData, ...newData]);
+          setNextPos(res.data.next);
+          setLoading(false);
+        });
+    };
+
+
     console.log('this is 1time runnind')
+    const onScroll = (event) => {
+      const { scrollHeight, scrollTop, clientHeight } =
+        event.target.scrollingElement;
+  
+      if (scrollHeight - scrollTop <= clientHeight * 1) {
+        featuredData();
+        setPage(page+1)
+        
+      }
+    };
     window.addEventListener('scroll', onScroll);
-  }, []);
+    featuredData();
+  },[]);
 
-  const featuredData = () => {
-    let newData = [];
-    axios
-      .get(
-        `https://tenor.googleapis.com/v2/featured?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c&pos=${nextPos}`
-      )
+ 
 
-      .then((res) => {
-        res.data.results.map((res) => newData.push(res));
-        setFeatured((oldData) => [...oldData, ...newData]);
-        setNextPos(res.data.next);
-        console.log('this is 2nd time running')
-      });
-  };
-
-  const onScroll = (event) => {
-    const { scrollHeight, scrollTop, clientHeight } =
-      event.target.scrollingElement;
-
-    if (scrollHeight - scrollTop <= clientHeight * 1) {
-      featuredData();
-      console.log('bottom of the page')
-      
-    }
-  };
+  
 
 
   return (
--- src/Search.js
+++ src/Search.js
@@ -3,9 +3,11 @@ import "./search.css";
 import whiteLogo from "./images/tenor-logo-white.svg";
 import { useState, useEffect } from "react";
 import searchlogo from "./images/search.svg";
+import axios from "axios";
 
 function Search() {
   const [fix, setFix] = useState(false);
+  const [sear,setSear] = useState([]);
 
   useEffect(() => {
     const setFixed = () => {
@@ -15,8 +17,21 @@ function Search() {
     };
 
     window.addEventListener("scroll", setFixed);
-  }, []);
 
+
+    
+  }, []);
+  const onType = async (val) => {
+    const search = val.target.value;
+    console.log(search)
+    axios
+    .get(`https://tenor.googleapis.com/v2/search?key=AIzaSyDTHKhc9ujnqiwS2M29T_p6cTVSq9N0a2c&q=${search}`)
+    .then((res) => {
+      setSear(res.data.results);
+    })
+  
+
+  }
   return (
     <div className="main-search">
       <div className="searh-contianer" style={{ marginTop: fix ? -37 : 0 }}>
@@ -25,6 +40,7 @@ function Search() {
           <input
             placeholder="Search for GIFs and Stickers"
             type="text"
+           onSubmit={onType}
             className="text-box"
             style={{ width: fix ? "53%" : "58%", marginLeft: fix ? 72 : 0 }}
           ></input>
@@ -33,6 +49,17 @@ function Search() {
           </div>
         </div>
       </div>
+
+      <div className="image-container">
+          <div className="coloum" ></div>
+          {sear.map((e,index) => {
+            return (
+              <div  className="images" key={e.id}> 
+                <img src={e.media_formats.gif.url} key={index.id} target="_blank"></img>
+              </div>
+            );
+          })}
+        </div>
     </div>
   );  
 }
